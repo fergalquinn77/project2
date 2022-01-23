cbfc029713226541aa203c1535b6e5b5bbd80608
fergalquinn77 <fergal@bourbon.ie>
2022-01-23 13:01:31 +0000

    Add reset function which resets the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-/**document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
+
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
@@ -7,109 +8,110 @@
             runGame(gameType);
         })
     }
-});*/
-
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("start").onclick = function () {
-        startGame()
-    };
+    document.getElementById("start").onclick = startGame;
+    document.getElementById("reset").onclick = gameReset;
 });
 
 
-const playerImage = document.getElementById("userChoice");
-const computerImage = document.getElementById("computerChoice");
-const moves = ["rock", "paper", "scissors", "spock", "lizard"];
-let computerMove = moves[Math.floor(Math.random() * moves.length)];
-
 function runGame(gameType) {
+
+    const moves = ["rock", "paper", "scissors", "spock", "lizard"];
+    let computerMove = moves[Math.floor(Math.random() * moves.length)];
+console.log(gameType);
+console.log(computerMove);
     if (gameType == "rock") {
         win = ["scissors", "lizard"];
         lose = ["spock", "paper"];
         if (win.includes(computerMove)) {
-            computerWin();
+            incrementScore("user");
         } else if (lose.includes(computerMove)) {
-            playerWin();
+            incrementScore("computer");
         } else {
             draw();
         }
+        updateImage(gameType,computerMove);
     }
     if (gameType == "scissors") {
         win = ["paper", "lizard"];
         lose = ["spock", "rock"];
         if (win.includes(computerMove)) {
-            computerWin();
+            incrementScore("user");
         } else if (lose.includes(computerMove)) {
-            playerWin();
+            incrementScore("computer");
         } else {
-            draw();
+            incrementScore("draw");
         }
+        updateImage(gameType,computerMove);
     }
     if (gameType == "lizard") {
         win = ["paper", "spock"];
         lose = ["scissors", "rock"];
         if (win.includes(computerMove)) {
-            computerWin();
+            incrementScore("user");
         } else if (lose.includes(computerMove)) {
-            playerWin();
+            incrementScore("computer");
         } else {
             draw();
         }
+        updateImage(gameType,computerMove);
     }
     if (gameType == "paper") {
         win = ["rock", "spock"];
         lose = ["scissors", "lizard"];
         if (win.includes(computerMove)) {
-            computerWin();
+            incrementScore("user");
         } else if (lose.includes(computerMove)) {
-            playerWin();
+            incrementScore("computer");
         } else {
             draw();
         }
+        updateImage(gameType,computerMove);
     }
     if (gameType == "spock") {
         win = ["scissors", "rock"];
         lose = ["lizard", "paper"];
         if (win.includes(computerMove)) {
-            computerWin();
+            incrementScore("user");
         } else if (lose.includes(computerMove)) {
-            playerWin();
+            incrementScore("computer");
         } else {
             draw();
         }
+        updateImage(gameType,computerMove);
     }
-    playerImage.src = `assets/images/${gameType}.jpg`;
-    playerImage.alt = `${gameType}`;
-    computerImage.src = `assets/images/${computer}.jpg`;
-    computerImage.alt = `${computer}`;
-    playerImage.src = `assets/images/${gameType}.jpg`;
-    computerImage.src = `assets/images/${computer}.jpg`;
+
 }
 
 function incrementScore(gameResult) {
+console.log(gameResult);
+    if (gameResult === "user") {
+        let oldScore = parseInt(document.getElementById('userScore').innerText);
+        document.getElementById('userScore').innerText = ++oldScore;
+        console.log("win");
+    } else if (gameResult === "computer") {
+        let oldScore = parseInt(document.getElementById('computerScore').innerText);
+        document.getElementById('computerScore').innerText = ++oldScore;
+        console.log("loose");
+    }
+    else {
 
-    let oldScore = parseInt(document.getElementById('score').innerText);
-    document.getElementById('score').innerText = oldScore++;
+    }
 
-    /**let oldScore = parseInt(document.getElementById('incorrect').innerText);
-    document.getElementById('incorrect').innerText = oldScore++;
-    */
+
 }
 
 function startGame() {
     if (document.getElementById("difficulty").value === "easy") {
-        document.getElementById("easy").style.display = "inline-block";
+        document.getElementById("easy").style.display = "inline-flex";
     } else {
-        document.getElementById("easy").style.display = "inline-block";
-        document.getElementById("difficult").style.display = "inline-block";
+        document.getElementById("easy").style.display = "inline-flex";
+        document.getElementById("difficult").style.display = "inline-flex";
     }
     document.getElementById("startControls").style.display = "none";
+    document.getElementById("choices").style.display = "inline-flex";
 }
 
 function difficulty() {}
-
-function computerWin() {}
-
-function playerWin() {}
 
 function draw() {}
 
@@ -117,4 +119,24 @@ function endGame() {}
 
 function checkScore() {}
 
-function draw() {}
+function updateImage(userInp,compInp){
+    let userImage = document.getElementById("userChoice");
+    let computerImage = document.getElementById("computerChoice");
+    userImage.src = `assets/images/${userInp}.jpg`;
+    userImage.alt = `${userInp}`;
+    computerImage.src = `assets/images/${compInp}.jpg`;
+    computerImage.alt = `${compInp}`;
+}
+
+function gameReset(){
+    let userImage = document.getElementById("userChoice");
+    let computerImage = document.getElementById("computerChoice");
+    userImage.src = 'assets/images/thumbs.jpg';
+    userImage.alt = 'user image';
+    computerImage.src = 'assets/images/computer.jpg';
+    computerImage.alt = 'computer image';
+    startGame();
+    document.getElementById('computerScore').innerText = 0;
+    document.getElementById('userScore').innerText = 0;
+
+}
